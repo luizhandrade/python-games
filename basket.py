a0d0a9dca568b7582ccf8c12e002ae6f2201e44d
Luiz Andrade <luizlhandrade@gmail.com>
2013-11-05 01:34:40 -0200

    Inclusão do arquivo "basket.py"

---
+++ basket.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+# Jogo teste em python
+#
+# Três frutas serão escolhidas, você escolhe uma 
+# e guarda numa cesta. Quando estiver com 3 frutas na cesta
+# o jogo verifica se há uma combinação. Se houver ganhará 
+# pontos com base no número de combinações possíveis.
+
+
